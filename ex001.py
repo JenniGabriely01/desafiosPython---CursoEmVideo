c130ef5ee16fbb63db277d4b520e7ce6ecd6a10a
Jennifer <lopesdossantosjennifergabriely@gmail.com>
2025-02-22 02:53:11 -0300

    Adicionando os dois primeiros desafios do curso

---
+++ ex001.py
@@ -0,0 +1,3 @@
+""" crie um programa que escreva 'Olá, mundo' na tela """
+print("Olá, mundo")
+""" no python pode usar tanto aspas simpels('') tanto aspas duplas (""), mas é mais comum se usar as simples """
---
+++ ex002.py
@@ -0,0 +1,3 @@
+nome = input("Qual é o seu nome?")
+""" print("Bem vindo(a),", nome) - pode se fazer assim tmb """
+print("bem vindo, {}!".format(nome)) 
